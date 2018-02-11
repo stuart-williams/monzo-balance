0b59a0cf3bcbe91b8a60143ad564c68b2e55fbc6
Stuart Williams <stuart.williams@bbc.co.uk>
2018-02-11 21:34:42 +0000

    Added css minification script

---
+++ scripts/css.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const { promisify } = require('util')
+const CleanCSS = require('clean-css')
+
+const writeFile = promisify(fs.writeFile)
+
+async function run () {
+  try {
+    const file = resolve(__dirname, '../public/main.css')
+    const output = new CleanCSS().minify([
+      resolve(__dirname, '../node_modules/normalize.css/normalize.css'),
+      resolve(__dirname, '../src/main.css')
+    ])
+    await writeFile(file, output.styles)
+    console.log(`Wrote to file ${file}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+run()
